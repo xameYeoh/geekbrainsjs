45906b585c36141fd7fc3f9401eb1281e5d1a292
getman <kgetmanskii@gmail.com>
2020-11-02 17:52:58 +0300

    first task

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let temperatureCelsius = prompt('Type in temperature in Celsius: ');
+let temperatureFahrenheit = (9 / 5) * temperatureCelsius + 32;
+alert(`Fahrenheit: ${temperatureFahrenheit}`);
+console.log(`${temperatureCelsius} celsius`);
+console.log(`${temperatureFahrenheit} fahrenheit`);
+
+let admin, name = 'Василий';
+admin = name;
+alert(`admin - ${admin}`);
+console.log(`${admin} - admin, ${name} - name`);
+let a=1000, b='108', c=a+b;
+console.log(c);
