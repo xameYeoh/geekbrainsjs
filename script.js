02c420c3a79c231259300d42a629ea966adb7daf
getman <kgetmanskii@gmail.com>
2020-11-20 20:30:27 +0300

    done 3 exercise

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const handleClick = (event) => {
     const src = event.target.getAttribute('src');
     const oldImg = document.querySelector('.full-picture-item');
-
+    const buttonToTheLeft = document.querySelector('.slider-left');
+    const buttonToTheRight = document.querySelector('.slider-right');
+    buttonToTheLeft.removeAttribute("hidden");
+    buttonToTheRight.removeAttribute("hidden");
     if (src === '' || src === '#') {
         console.log("The src of picture is empty!");
         return;
@@ -27,21 +30,58 @@ const handleClick = (event) => {
     }
 }
 
-const handleMouseEnter = (event) => {
-    console.log("Mouse entered!");
-    event.target.classList.add('active')
-}
-
-const handleMouseLeave = (event) => {
-    console.log("Mouse left!");
-    event.target.classList.remove('active')
+getImageNumber = (src) => {
+    const previews = document.querySelectorAll('img.preview-item');
+    for (let i = 0; i < previews.length; i++) {
+        if (previews[i].getAttribute("src") === src) {
+            console.log(i);
+            return i;
+        }
+    }
+    return false;
 }
 
 window.onload = () => {
     const previews = document.querySelectorAll('img.preview-item');
+    const wrapper = document.querySelector('.full-picture');
     for (const item of previews) {
         item.addEventListener('click', handleClick);
-        item.addEventListener('mouseenter', handleMouseEnter);
-        item.addEventListener('mouseleave', handleMouseLeave);
     }
+    const buttonToTheLeft = document.querySelector('.slider-left');
+    const buttonToTheRight = document.querySelector('.slider-right');
+
+
+    buttonToTheLeft.onclick = () => {
+        const currentImageSrc = document.querySelector('.full-picture-item').getAttribute("src");
+
+        let i = getImageNumber(currentImageSrc);
+        if (previews[i - 1]) {
+            const src = previews[i - 1].getAttribute('src');
+            const oldImg = document.querySelector('.full-picture-item');
+            const img = document.createElement('img');
+            img.classList.add("full-picture-item");
+            img.setAttribute('src', src);
+
+            wrapper.replaceChild(img, oldImg);
+        }
+
+    }
+
+    buttonToTheRight.onclick = () => {
+        const currentImageSrc = document.querySelector('.full-picture-item').getAttribute("src");
+
+        let i = getImageNumber(currentImageSrc);
+        if (previews[i + 1]) {
+            const src = previews[i + 1].getAttribute('src');
+            const oldImg = document.querySelector('.full-picture-item');
+            const img = document.createElement('img');
+            img.classList.add("full-picture-item");
+            img.setAttribute('src', src);
+
+            wrapper.replaceChild(img, oldImg);
+        }
+
+    }
+
+
 }
